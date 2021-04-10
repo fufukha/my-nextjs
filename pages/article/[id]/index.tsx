1c73cd681fefb7a40a3024f19e600a08b894ac61
fufukha <wafakhalid29@gmail.com>
2021-04-10 11:59:50 -0400

    used StaticProps and StaticPaths instead of ServerSideProps

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,6 +1,5 @@
-import { useRouter } from 'next/router'
-import { GetServerSideProps } from 'next'
-import { Article } from '../../../types/article-types'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { Article, IParams } from '../../../types/article-types'
 import Link from 'next/link'
 
 type ArticleProp = {
@@ -18,7 +17,7 @@ const article = ({ article }: ArticleProp) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   )
@@ -31,6 +30,35 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   }
 }
+
+export const getStaticPaths: GetStaticPaths<IParams> = async () => {
+  //creates a path for all the article even outside of the 6
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+  const articles: Article[] = await res.json()
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    //paths: params: {id: '1', id: '2',...}
+    paths,
+    fallback: false,
+  }
+}
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   )
+
+//   const article = await res.json()
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   }
+// }
 
 // const article = () => {
 //   const router = useRouter()
--- types/article-types.ts
+++ types/article-types.ts
@@ -1,6 +1,12 @@
+import { ParsedUrlQuery } from 'querystring'
+
 export interface Article {
   userId: number
   id: number
   title: string
   body: string
+}
+
+export interface IParams extends ParsedUrlQuery {
+  id: string
 }
