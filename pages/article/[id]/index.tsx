8a5a7c7729c27bad9a75a9e658189fe06be6c6b8
fufukha <wafakhalid29@gmail.com>
2021-04-10 11:11:46 -0400

    fetching article data with serversideprops

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { GetServerSideProps, GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import { Article } from '../../../types/article-types'
 import Link from 'next/link'
 
@@ -7,14 +7,39 @@ type ArticleProp = {
   article: Article
 }
 
-const article = () => {
-  const router = useRouter()
-  const { id } = router.query
+const article = ({ article }: ArticleProp) => {
   return (
-    <div>
-      This is an article {id}
-    </div>
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
+
+// const article = () => {
+//   const router = useRouter()
+//   const { id } = router.query
+//   return (
+//     <div>
+//       This is an article {id}
+//     </div>
+//   )
+// }
 
 export default article
